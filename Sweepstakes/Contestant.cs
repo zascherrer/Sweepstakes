3c05424553fb2de4bbca3ee715182ac9f8a3c4eb
Zachary Scherrer <zachary.a.scherrer@gmail.com>
2018-07-10 14:38:58 -0500

    Created the Contestant and UserInterface classes.

---
+++ Sweepstakes/Contestant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    public class Contestant
+    {
+        public string firstName;
+        public string lastName;
+        public string email;
+        public Guid registrationNumber;
+
+        public Contestant()
+        {
+            firstName = "John";
+            lastName = "Doe";
+            email = "john.doe@gmail.com";
+            registrationNumber = new Guid();
+        }
+
+        public Contestant(string firstName, string lastName, string email)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            registrationNumber = new Guid();
+        }
+    }
+}
---
+++ Sweepstakes/UserInterface.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    public static class UserInterface
+    {
+
+
+        public static void EnterContestant(Dictionary<Guid, Contestant> registration, List<Guid> registrationNumbers)
+        {
+            Contestant contestant = EnterContestantInformation();
+            int numberOfTickets = SetNumberOfTickets();
+            
+            for(int i = 0; i < numberOfTickets; i++)
+            {
+                Contestant newTicket = new Contestant(contestant.firstName, contestant.lastName, contestant.email);
+                registrationNumbers.Add(newTicket.registrationNumber);
+                registration.Add(newTicket.registrationNumber, newTicket);
+            }
+        }
+
+        public static Contestant EnterContestantInformation()
+        {
+            Contestant contestant = new Contestant();
+
+            contestant.firstName = GetUserInput("Please enter your first name: ");
+            contestant.lastName = GetUserInput("Please enter your last name: ");
+            contestant.email = GetUserInput("Please enter your email address: ");
+
+            return contestant;
+        }
+
+        private static string GetUserInput(string message)
+        {
+            Console.WriteLine("/n" + message);
+            string input = Console.ReadLine();
+            return input;
+        }
+
+        private static int SetNumberOfTickets()
+        {
+            string numberOfTicketsString = GetUserInput("How many tickets would you like to buy?");
+            int numberOfTickets = 1;
+            bool numberHasBeenEntered = int.TryParse(numberOfTicketsString, out numberOfTickets);
+
+            return numberOfTickets;
+        }
+    }
+}
